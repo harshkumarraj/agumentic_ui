661decd05d2576f2aac0c8d5a40b4a4084e5ad1f
harshkumarraj <hraj9503@gmail.com>
2023-07-31 01:00:47 +0530

    Final commit

--- src/components/Component/Components.js
+++ src/components/Component/Components.js
@@ -2,39 +2,117 @@
 import React from 'react';
 import CycleParts from './../../assets/cycleParts.png';
 import './Components.css'; // Import the CSS file for Components
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 const Components = () => {
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [img, setImg] = useState([]);
+
+
+  const [data2, setData2] = useState([]);
+  const [title2, setTitle2] = useState([]);
+  const [img2, setImg2] = useState([]);
+  const [name2, setName2] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://agumentic-backend-r34f.onrender.com/get3/')
+      .then(res => {
+        setData2(res.data);
+        console.log(res.data);
+        setTitle2(res.data[0].title);
+        setImg2(res.data[0].img);
+        setName2(res.data[0].name);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+
+  useEffect(() => {
+    axios.get('https://agumentic-backend-r34f.onrender.com/get2/')
+      .then(res => {
+        setData(res.data);
+        console.log(res.data);
+        setTitle(res.data[0].title);
+        setImg(res.data[0].img);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+
+  const [img3, setImg3] = useState([]);
+  const [name3, setName3] = useState([]);
+  const[img4,setImg4]=useState([]);
+  const[name4,setName4]=useState([]);
+  const[img5,setImg5]=useState([]);
+  const[name5,setName5]=useState([]);
+  const[img6,setImg6]=useState([]);
+  const[name6,setName6]=useState([]);
+  const[img7,setImg7]=useState([]);
+  const[name7,setName7]=useState([]);
+
+  useEffect(() => {
+    axios.get('https://agumentic-backend-r34f.onrender.com/get3/')
+      .then(res => {
+        setData(res.data);
+        console.log(res.data);
+        setImg3(res.data[1].img);
+        setName3(res.data[1].name);
+        setImg4(res.data[2].img);
+        setName4(res.data[2].name);
+        setImg5(res.data[3].img);
+        setName5(res.data[3].name);
+        setImg6(res.data[4].img);
+        setName6(res.data[4].name);
+        setImg7(res.data[5].img);
+        setName7(res.data[5].name);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+
+
+
   return (
     <div>
       <p style={{ fontSize: '40px', textAlign: 'center', marginTop: '200px' }}>
-        <b>FEATURE OF NINER RLT 9 RDO 5</b>
+        <b>
+          {/* FEATURE OF NINER RLT 9 RDO 5 */}
+          {title && <p>{title}</p>}
+          </b>
       </p>
-      <img src={CycleParts} alt="Bicycle" style={{ width: '90%', height: '100%', marginLeft: '80px' }} />
+      <img src={img} alt="Bicycle" style={{ width: '90%', height: '100%', marginLeft: '80px' }} />
       <p style={{ fontSize: '40px', textAlign: 'center', marginTop: '150px' }}>
         <b>FEATURED PRODUCTS</b>
       </p>
       <p className="fade-text" style={{ fontSize: '15px', textAlign: 'center' }}>
         We also carry a wide range of accessories, including helmets, locks, lights, and <br />
         clothing, so you can be sure you're fully equipped for your next ride.
+       
       </p>
 
       {/* First row of cards */}
       <div className="card-container">
         <div className="card">
-          <img src="https://m.media-amazon.com/images/I/71u0LdBfzRL.jpg" style={{height:'240px'}} alt="Product 1" />
-          <p style={{fontSize:'30px'}}>Hero Sprint 1</p>
+          <img src={img2} style={{height:'240px'}} alt="Product 1" />
+          <p style={{fontSize:'30px'}}>{name2 && <h4> {name2}</h4> }</p>
           <p>$4,510</p>
           <button style={{backgroundColor:'#ef740f',color:'white'}}>SHOP NOW</button>
         </div>
         <div className="card">
-          <img src="https://m.media-amazon.com/images/I/81Fq8U49OoS._SL1500_.jpg" style={{height:'240px'}} alt="Product 2" />
-          <p style={{fontSize:'30px'}}>Hero Sprint 2</p>
+          <img src={img3} style={{height:'240px'}} alt="Product 2" />
+          <p style={{fontSize:'30px'}}> {name3 && <h4>{name3}</h4> }  </p>
           <p>$1,570</p>
           <button>SHOP NOW</button>
         </div>
         <div className="card">
-          <img src="https://rukminim1.flixcart.com/image/850/1000/xif0q/cycle/h/n/6/cycles-catcher-non-suspension-v-brake-single-speed-black-orange-original-imagg359hkdujbdh.jpeg?q=90" style={{height:'240px'}} alt="Product 3" />
-          <p style={{fontSize:'30px'}}>Hero Sprint 3</p>
+          <img src={img4} style={{height:'240px'}} alt="Product 3" />
+          <p style={{fontSize:'30px'}}>{name4 && <h4>{name4}</h4> }</p>
           <p>$3,110</p>
           <button>SHOP NOW</button>
         </div>
@@ -43,20 +121,20 @@ const Components = () => {
       {/* Second row of cards */}
       <div className="card-container">
         <div className="card">
-          <img src="https://rukminim1.flixcart.com/image/850/1000/xif0q/cycle/c/1/2/sprint-x-factor-26-t-single-speed-mountain-bicycle-with-front-original-imagnb4dzppyz7j4.jpeg?q=20" style={{height:'240px'}} alt="Product 4" />
-          <p style={{fontSize:'30px'}}>Hero Sprint 4</p>
+          <img src={img5} style={{height:'240px'}} alt="Product 4" />
+          <p style={{fontSize:'30px'}}>{name5 && <h4>{name5}</h4> }</p>
           <p>$6,220</p>
           <button>SHOP NOW</button>
         </div>
         <div className="card">
-          <img src="https://s3.ap-south-1.amazonaws.com/choosemybicycle.webp/images/bicycles/hero-sprint-traveller-700c-7-speed.webp" style={{height:'240px'}} alt="Product 5" />
-          <p style={{fontSize:'30px'}}>Hero Sprint 5</p>
+          <img src={img6} style={{height:'240px'}} alt="Product 5" />
+          <p style={{fontSize:'30px'}}>{name6 && <h4>{name6}</h4> }</p>
           <p>$9,999</p>
           <button>SHOP NOW</button>
         </div>
         <div className="card">
-          <img src="https://images.jdmagicbox.com/quickquotes/images_main/hero-bicycle-2008352433-8yyco9ea.jpg" style={{height:'240px'}} alt="Product 6" />
-          <p style={{fontSize:'30px'}}>Hero Sprint 6</p>
+          <img src={img7} style={{height:'240px'}} alt="Product 6" />
+          <p style={{fontSize:'30px'}}>{name7 && <h4>{name7}</h4> }</p>
           <p>$5,150</p>
           <button>SHOP NOW</button>
         </div>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,15 +2,41 @@
 import React from 'react';
 import './Hero.css'; // Import the CSS file for Hero component
 import BicycleImage from './../../assets/bicycleimage.png'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 
 const Hero = () => {
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [img, setImg] = useState([]);
+  const [description, setDescription] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('https://agumentic-backend-r34f.onrender.com/get1/')
+      .then(res => {
+        setData(res.data);
+        console.log(res.data);
+        setTitle(res.data[0].title);
+        setImg(res.data[0].img);
+        setDescription(res.data[0].description);
+      })
+      .catch(err => {
+        console.log(err);
+      })  
+  }, []);
+
   return (
     <div className="hero">
       <div className="hero__left" style={{paddingLeft:'80px'}}>
-        <h1 style={{fontSize:'50px'}}>NINER RLT 9 RDO 5- <br/>STAR SHIMANO GRX<br/> 800 1X</h1>
+        <h1 style={{fontSize:'50px'}}>
+          {title && <p>{title}</p>}
+          </h1>
         <p>
-          From bikes, gear, and accessories to expert advice and<br/>reviews, a bicycle website is your one-stop-shop for all your<br/> cycling needs.
+          {/* From bikes, gear, and accessories to expert advice and<br/>reviews, a bicycle website is your one-stop-shop for all your<br/> cycling needs. */}
+          {description && <p>{description}</p>}
         </p>
         <div className="hero__button-price-wrapper">
           <a href="#shop" className="hero__button">SHOP NOW</a>
@@ -18,7 +44,7 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero__right">
-        <img src={BicycleImage} alt="Bicycle" className="hero__bicycle-image" />
+        <img src={img} alt="Bicycle" className="hero__bicycle-image" />
         <div className="hero__banner"><b>TO 50% DISCOUNT FOR FIRST &nbsp;  ● &nbsp;  UPTO 50% DISCOUNT FOR FIRST ORDER &nbsp;  ● &nbsp;  UPTO 50% DISCOUNT</b></div>
       </div>
     </div>
